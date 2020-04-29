2df2ee970fc1be7501133058b74df2580535fda0
DxoxS <63910568+DxoxS@users.noreply.github.com>
2020-04-30 02:02:32 +0900

    3週目の練習問題が最後まで終わりました。
    テキストの通りやったつもりなのですが、完成のデザイン通りにならなかったので少し手を加えて完成デザインに見えるようにしました。ご確認お願いいたします。

--- 3週目(jQuery)/js/main.js
+++ 3週目(jQuery)/js/main.js
@@ -1,6 +1,20 @@
 $(function(){
     $('#hamburger').click(function(){
         $(this).toggleClass("active");
-        $(".menu-content").toggleClass("open")
+        $('.menu-content').toggleClass("open");
+    })
+
+    $('#top-btn').click(function(){
+        $('html,body').animate({
+            scrollTop:0
+        },700);
+    })
+
+    const child =".child";
+    $(child).hide();
+    $("#menu > li").hover(function(){
+        $(child,this).stop().slideDown(300);
+    }, function(){
+        $(child,this).stop() .slideUp(300);
     })
 });
